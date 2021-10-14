541500e0d783331d1769ebb727ff7231489c1dca
Johri van Gastelen <johri.van.gastelen@gmail.com>
2021-10-14 14:00:18 +0200

    The Content Class - Exercise 11 & 12

--- ssg/content.py
+++ ssg/content.py
@@ -36,3 +36,10 @@ class Content(Mapping):
 
     def __len__(self):
         return len(self.data)
+    
+    def __repr__(self):
+        data = {}
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
+        return str(data)
